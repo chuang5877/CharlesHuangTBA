7f5afa10e6651fd036ff51f61ae5a6253686be5c
chuang5877 <chuang5877@bths.edu>
2018-12-05 08:38:33 -0500

    ok

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,18 +1,16 @@
 package com.company;
 
 public class Board {
-    String[][] Board;
-    private int x;
-    private int y;
-    public Board(String difficulty){
-        if(difficulty.equalsIgnoreCase("Easy")){
-            this.Board=new String[5][5];
-        }else if(difficulty.equalsIgnoreCase("Normal")){
-            this.Board=new String[7][7];
-        }else if(difficulty.equalsIgnoreCase("Hard")){
-            this.Board=new String[9][9];
-        }else{
-
+    Room[][] board;
+    public Board(int h, int w){
+        this.board=new Room[h][w];
+    }
+    public void printBoard(){
+        for(Room[] i:board){
+            for(Room n:i){
+                System.out.print(n);
+            }
+            System.out.print('\n');
         }
     }
 }
