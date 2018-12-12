bc35372d2dd93e9bcd9580a3421b2e89525834c8
chuang5877 <chuagn5877@bths.edu>
2018-12-12 08:37:02 -0500

    Skeleton

---
+++ src/com/company/Rooms/Room.java
@@ -0,0 +1,17 @@
+package com.company.Rooms;
+
+import com.company.Person;
+
+public class Room {
+    Person p1;
+    int x;
+    int y;
+    public void enterRoom(Person p){
+        p1=p;
+        p.setX(this.x);
+        p.setY(this.y);
+    }
+    public void leaveRoom(Person p){
+        p1=null;
+    }
+}
---
+++ src/com/company/Rooms/Skeleton.java
@@ -0,0 +1,29 @@
+package com.company.Rooms;
+
+import com.company.Main;
+import com.company.Person;
+import java.util.Scanner;
+
+public class Skeleton extends Room {
+
+    public Skeleton(int x,int y){
+
+    }
+    public void enterRoom(Person x){
+        p1=x;
+        x.setX(this.x);
+        x.setY(this.y);
+        System.out.print("You enter a room with a skeleton. The skeleton approaches you. Do you want to attack?");
+        Scanner in=new Scanner(System.in);
+        String i=in.nextLine();
+        if(i.equalsIgnoreCase("yes")){
+            double coin=Math.random();
+            if(coin<=.5){
+                System.out.println("You bleed to death on the filthy dungeon floor");
+                Main.gameOff();
+            }else{
+                System.out.println("The skeleton falls apart. You wouldn't want to wait to see if it could rebuild itself");
+            }
+        }
+    }
+}
