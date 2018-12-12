bd47d6d3fe86829f6b3bbae17669a42dcf559793
chuang5877 <chuagn5877@bths.edu>
2018-12-12 08:35:43 -0500

    Skeleton

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,14 +1,17 @@
 package com.company;
 
+import com.company.Rooms.Room;
+
 public class Board {
     Room[][] board;
     public Board(int h, int w){
         this.board=new Room[h][w];
     }
     public void printBoard(){
+        String s="[ ]";
         for(Room[] i:board){
             for(Room n:i){
-                System.out.print(n);
+                System.out.print(s);
             }
             System.out.print('\n');
         }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static boolean gameOn = true;
 
     public static void main(String[] args) {
-        
+        Board b1=new Board(4,4);
+        b1.printBoard();
+        Person p1=new Person(0,0);
+        Scanner in=new Scanner(System.in);
+    }
+    public static void gameOff(){
+        gameOn = false;
     }
 }
+
